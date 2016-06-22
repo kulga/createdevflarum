28fbbcbb59b4b00fbb1a3157a3a37060adcd7fca
kulga <nzrvrq_github_kulga@Kurzepost.de>
2016-06-22 08:52:45 -0700

    Create config_default.php

---
+++ docs/config_default.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' =>
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => '',
+  'paths' =>
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
